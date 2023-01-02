0bbc74b211e447db2e3e1d54e4ea844efff97855
Nelson Junior <nsilva.solks@digio.com.br>
2023-01-01 22:41:02 -0300

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Junior Silva on 01/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
